dfb8cee1fc04fb1acd4b82f250ce122dbafbb116
Rohit Kanrar <rohitk@iastate.edu>
2023-12-28 20:13:56 +0530

    subtracted constant 1

--- code/r/generate_data/moment_shift.R
+++ code/r/generate_data/moment_shift.R
@@ -1,20 +1,20 @@
 get_dense_shift_normal_moment <- function(n, p, prop = 0.2, tau = 0.5){
   t0 <- floor(n*tau)
   d <- floor(p*prop)
-  s1 <- matrix(rnorm(t0*p), t0, p) + 1
-  s2 <- matrix(rnorm((n-t0)*p), (n-t0), p) + 1
+  s1 <- matrix(rnorm(t0*p), t0, p)
+  s2 <- matrix(rnorm((n-t0)*p), (n-t0), p)
   for(j in 1:d)
-    s2[, j] <- rexp(n-t0)
+    s2[, j] <- rexp(n-t0) - 1
   rbind(s1, s2)
 }
 
 get_sparse_shift_normal_moment <- function(n, p, prop = 0.01, tau = 0.5){
   t0 <- floor(n*tau)
   d <- floor(p*prop)
-  s1 <- matrix(rnorm(t0*p), t0, p) + 1
-  s2 <- matrix(rnorm((n-t0)*p), (n-t0), p) + 1
+  s1 <- matrix(rnorm(t0*p), t0, p)
+  s2 <- matrix(rnorm((n-t0)*p), (n-t0), p)
   for(j in 1:d)
-    s2[, j] <- rexp(n-t0)
+    s2[, j] <- rexp(n-t0) - 1
   rbind(s1, s2)
 }
 
