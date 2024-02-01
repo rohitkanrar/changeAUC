030a1acdaefb373d7a52c2284528840fd7a92aaa
Rohit Kanrar <rohitk@iastate.edu>
2024-02-01 17:28:26 -0600

    removed matplotlib from misc

--- code/py/real_data/nyc_taxi/get_data/generate_heatmaps.py
+++ code/py/real_data/nyc_taxi/get_data/generate_heatmaps.py
@@ -1,11 +1,46 @@
 import pyarrow.parquet as pq
 import pandas as pd
+from matplotlib import colors
+import matplotlib.pyplot as plt
 import sys, os
+import pickle as pkl
+import numpy as np
 import geopandas as gpd
+
 sys.path.insert(0, "./code/py")
 sys.path.insert(0, "./data")
-from misc.misc_v1 import generate_df_from_record
-from misc.misc_v1 import generate_daily_heatmaps_from_df
+sys.path.insert(0, "./output")
+
+
+def generate_df_from_record(dat):
+    dat = dat[~dat['DOlocationID'].isna()]
+    dat['year_month_day'] = dat['pickup_datetime'].dt.strftime("%y-%m-%d")
+    del dat['pickup_datetime']
+    dat['trip'] = 1
+    dat['DOlocationID'] = dat['DOlocationID'].astype(int)
+    dat_agg = dat.groupby(['year_month_day', 'DOlocationID']).agg({'trip': 'sum'})
+    del dat
+    dat_agg.reset_index(inplace=True)
+    return dat_agg
+
+
+with open("output/real_data/nyc_taxi/trip_counts.pkl", "rb") as fp:
+    trip_counts = pkl.load(fp)
+trip_min = int(np.min(trip_counts))
+trip_median = int(np.median(trip_counts))
+
+
+def generate_daily_heatmaps_from_df(df, map, save_dir):
+    divnorm = colors.TwoSlopeNorm(vmin=trip_min, vcenter=trip_median)
+    for i in df['year_month_day'].unique():
+        one_day = df[df['year_month_day'] == i]
+        ax = map.merge(one_day, how='left', left_on='LocationID', right_on='DOlocationID').fillna(0).plot(column='trip',
+                                                                                                          cmap='Greys',
+                                                                                                          norm=divnorm)
+        plt.axis('off')
+        ax.figure.savefig(save_dir + '/' + i + '.jpg', format='jpg',
+                          bbox_inches='tight', pad_inches=0)
+        plt.close()
 
 
 nyc_map = gpd.read_file("data/fhv_nyc/taxi_zones.zip")
