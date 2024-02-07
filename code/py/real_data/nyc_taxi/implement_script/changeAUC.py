4a5268c7ef3c23dc068ea86595df6632921a5ade
Rohit Kanrar <rohitk@iastate.edu>
2024-02-06 19:01:11 -0600

    changed to full taxi data

--- code/py/real_data/nyc_taxi/implement_script/changeAUC.py
+++ code/py/real_data/nyc_taxi/implement_script/changeAUC.py
@@ -26,19 +26,10 @@ with open(root_dir + "data/fhv_nyc/heatmaps_numeric.pkl", "rb") as f:
 
 random.seed(100)
 
-# date_list = sorted(os.listdir(heatmap_dir))
-# oct_start = date_list.index("22-10-01-00.jpg")
-# nov_start = date_list.index("22-11-01-00.jpg")
-# dec_start = date_list.index("22-12-01-00.jpg")
-# jan_start = date_list.index("23-01-01-00.jpg")
-# jan_end = date_list.index("23-01-31-23.jpg")
-
-detect_global_multiple = get_multiple_change_point(heatmaps_array[365:1461], classifier="VGG16", split_trim=0.15, auc_trim=0.05,
+detect_global_multiple = get_multiple_change_point(heatmaps_array, classifier="VGG16", split_trim=0.15, auc_trim=0.05,
                                                    no_of_perm=199, min_length=500, decay=np.sqrt(2),
                                                    return_output="all")
 
-with open(out_dir + "vgg16_sbs_nyc_taxi_short.pkl", "wb") as fp:
+with open(out_dir + "vgg16_sbs_nyc_taxi.pkl", "wb") as fp:
     pkl.dump(detect_global_multiple, fp)
 
-
-# print(get_change_point(heatmaps_array, classifier='VGG16'))
