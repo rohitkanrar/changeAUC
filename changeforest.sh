1c245d44a4cf72e26dedaa59f431735172c7c00c
rohitkanrar <rohitk@iastate.edu>
2024-10-07 11:30:40 -0500

    minor change

---
+++ changeforest.sh
@@ -0,0 +1,17 @@
+python code/py/simulation_scripts/simulation_change_forest.py -d 2 -n 1000 -p 500 -r 500 -g dense_mean -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 2 -n 1000 -p 500 -r 500 -g sparse_mean -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 0.1 -n 1000 -p 500 -r 500 -g dense_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 0.8 -n 1000 -p 500 -r 500 -g sparse_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 5 -n 1000 -p 500 -r 500 -g dense_diag_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 5 -n 1000 -p 500 -r 500 -g sparse_diag_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 1 -n 1000 -p 500 -r 500 -g dense_moment -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 1 -n 1000 -p 500 -r 500 -g sparse_moment -l local -s 1
+
+python code/py/simulation_scripts/simulation_change_forest.py -d 2 -n 1000 -p 1000 -r 500 -g dense_mean -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 2 -n 1000 -p 1000 -r 500 -g sparse_mean -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 0.1 -n 1000 -p 1000 -r 500 -g dense_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 0.8 -n 1000 -p 1000 -r 500 -g sparse_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 5 -n 1000 -p 1000 -r 500 -g dense_diag_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 5 -n 1000 -p 1000 -r 500 -g sparse_diag_cov -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 1 -n 1000 -p 1000 -r 500 -g dense_moment -l local -s 1
+python code/py/simulation_scripts/simulation_change_forest.py -d 1 -n 1000 -p 1000 -r 500 -g sparse_moment -l local -s 1
