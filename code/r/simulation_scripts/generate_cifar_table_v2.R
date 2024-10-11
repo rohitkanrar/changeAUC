c0f5a3afd17eb78dcb0316c683cd9432fa593400
rohitkanrar <rohitk@iastate.edu>
2024-10-11 10:29:15 -0500

    modified cifar table

--- code/r/simulation_scripts/generate_cifar_table_v2.R
+++ code/r/simulation_scripts/generate_cifar_table_v2.R
@@ -1,10 +1,11 @@
+source("code/r/misc/misc_v1.R")
 epsilon <- 0.15
 eta <- 0.05
 source("code/r/get_null_quantiles/combine.R")
 c95 <- q95/sqrt(1000) + 0.5
 dgp <- c("3-5", "4-5", "4-7", "4-4", "5-5")
 method_ <- c("gseg", "vgg16", "vgg19")
-final_cifar_table <- matrix(0, length(dgp), length(method_)+3)
+final_cifar_table <- matrix(0, length(dgp)+2, length(method_)+3)
 library(reticulate)
 pd <- import("pandas")
 
@@ -57,19 +58,28 @@ for(dgp_ in dgp){
       }
       j <- 4
     } else{
-      ari <- numeric(0)
       size <- numeric(0)
-      for(file_ in list.files(file_dir)){
-        file__ <- paste(file_dir, file_, sep = "")
-        out <- pd$read_pickle(file__)
-        if(i <= 3){
-          ari <- c(ari, ifelse(out$max_auc >= c95, out$ari, 0))
-        } else{
-          size <- c(size, ifelse(out$max_auc >= c95, 1, 0))
-        }
-      }
+      # for(file_ in list.files(file_dir)){
+      #   file__ <- paste(file_dir, file_, sep = "")
+      #   out <- pd$read_pickle(file__)
+      #   if(i <= 3){
+      #     ari <- c(ari, ifelse(out$max_auc >= c95, out$ari, 0))
+      #   } else{
+      #     size <- c(size, ifelse(out$max_auc >= c95, 1, 0))
+      #   }
+      # }
+      file_ <- paste(file_dir, "cifar_", dgp_, "_n_1000_rep_500.pkl", sep = "")
+      out <- pd$read_pickle(file_)
+      ari <- out$ari
+      ch_pt_cusum <- apply(out$cusums, 1, which.max) + 
+        out$n * (out$split_trim + out$auc_trim)
+      ari_cusum <- sapply(1:length(ch_pt_cusum), function(i){
+        cp <- ch_pt_cusum[i]
+        get_ari(n = out$n, true_ch_pt = floor(out$n / 2), ch_pt = cp)
+      })
       if(i <= 3){
         final_cifar_table[i, j] <- mean(ari)
+        final_cifar_table[i, j+2] <- mean(ari_cusum)
       } else{
         final_cifar_table[i, j] <- mean(size)
       }
